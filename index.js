7df3454c0e127f7ce968ec8fa372f587e03e1bd1
unknown <evgen.bond98@gmail.com>
2025-05-06 15:28:55 +0300

    Доробив збереження aih. В процесі acth. Додав динамічне введеня адрес DB через інтерфейс консолі

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const fs = require ('fs');
+const readline = require('readline');
 const path = require('path');
 const dbfolder = './dbdir/';
 const jsonfolder = './jsondir/';
+
+var data = require("./data.json")
+var tags = {}; // об'єкт для тегів FUXA
+
+var prefix = 0; // номер девайсу (формування префіксу)
+var dbAdress = {}; // об'єкт для адрес DB
+
+// Створюємо інтерфейс для вводу з консолі
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Функція для запиту адреси DB у користувача
+const askForDbAddress = async (dbTypes) => {
+  const dbAdress = {};
+  for (const dbType of dbTypes) {
+    await new Promise((resolve) => {
+      rl.question(`Введіть адресу для DB типу ${dbType}: `, (input) => {
+        dbAdress[dbType] = parseInt(input, 10); // Зберігаємо адресу як число
+        resolve();
+      });
+    });
+  }
+  return dbAdress;
+};
+
+// Функція для запиту prefix у користувача
+const askForPrefix = async () => {
+  return new Promise((resolve) => {
+    rl.question('Введіть номер установки (prefix): ', (input) => {
+      resolve(parseInt(input, 10)); // Зберігаємо prefix як число
+    });
+  });
+};
 
 // Code in function ParseDBtoJson get from pacframework-tools
 // Source: https://github.com/pupenasan/pacframework-tools
@@ -50,17 +86,8 @@ const ParseDBtoJson = function (dbfolder,jsonfolder, callback) {
       
       callback(objects);
     });
+
 }
-
-const data = require("./data.json")
-
-const devId = 0; // номер девайсу (формування префіксу)
-const dbAdress = { // адреса DB в ПЛК
-  ACTH:3,
-  AIH:4,
-  AOH:5,
-  DIH:6,
-  DOH:7};
 
 const CreateFuxaTags = function(object){
     
@@ -74,35 +101,107 @@ const CreateFuxaTags = function(object){
         for (let i = 0; i < Object.keys(object[dbName]).length; i++) {
           var elements = Object.keys(object[dbName]);
             
-          fuxaTag[`1${dbName}_${elements[i]}_STA`] = {} // Формуємо назву тега для FUXA
+          fuxaTag[`${prefix}${dbName}_${elements[i]}_STA`] = {} // Формуємо назву тега для FUXA
           
-          // Створити вибір та наповнення в залежності від типу тега, для того щоб правильно порахувати адресу та зміщення байт
-          // Порахувати адресу
 
-          console.log(fuxaTag);
         }
         break;
       case "AIH":
+        var y = -6; // Зміщення для STA
+        var u = 0; // Зміщення для VAL
+
         for (let i = 0; i < Object.keys(object[dbName]).length; i++) {
+          
           var elements = Object.keys(object[dbName]);
-          console.log(`1${dbName}_${elements[i]}`);
-        }
+          
+          fuxaTag[`${prefix}${dbName}_${elements[i]}_STA`] = { // Формуємо тег AIH_STA
+            "id": `${prefix}${elements[i]}_STA`,
+            "daq": {
+              "restored": false,
+              "enabled": false,
+              "changed": false,
+              "interval": 60
+            },
+            "name": `${prefix}${dbName}_${elements[i]}_STA`,
+            "type": "DWord",
+            "address": adress = `db${dbAdress.AIH}.dbw${y + 8}`,
+            "description": object[dbName][elements[i]].descr
+          }
+          y=y + 8;
+          
+          fuxaTag[`${prefix}${dbName}_${elements[i]}_VAL`] = { // Формуємо тег AIH_VAL
+            "id": `${prefix}${elements[i]}_VAL`,
+            "daq": {
+              "restored": false,
+              "enabled": false,
+              "changed": false,
+              "interval": 60
+            },
+            "name": `${prefix}${dbName}_${elements[i]}_VAL`,
+            "type": "Real",
+            "address": adress = `db${dbAdress.AIH}.dbd${u + 8}`,
+            "description": object[dbName][elements[i]].descr
+          }
+          u=u + 8;
+        
+          tags = {...tags, ...fuxaTag}; // Додаємо теги в загальний об'єкт tags
+                 
+        };
         break;
-    
+        
       default:
         break;
+        
     }
     
-    
-
-
-    //data[1].tags[`1${dbName}`] = object[dbName];
-    //console.log(data);
   };
-  //console.log(data[1].tags.ACT)
-}
 
-;
+};
 
-ParseDBtoJson(dbfolder,jsonfolder,CreateFuxaTags);
+
+// ParseDBtoJson(dbfolder, jsonfolder, function (objects) {
+//   // Викликаємо CreateFuxaTags і передаємо об'єкт
+//   CreateFuxaTags(objects);
+
+//   data[1].tags = tags; // Додаємо теги в загальний об'єкт data
+
+//   fs.writeFileSync('./new.json', JSON.stringify(data, null, 2), 'utf8');
+//   console.log("Оновлений data збережено у файл.");
+// });
+
+
+fs.readdir(path.join(__dirname, dbfolder), async function (err, files) {
+
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  // Визначаємо типи DB за назвами файлів
+  const dbTypes = files
+    .filter((file) => path.extname(file) === '.db') // Фільтруємо тільки файли з розширенням .db
+    .map((file) => path.basename(file, '.db')); // Отримуємо назви файлів без розширення
+
+  console.log('Знайдені типи DB:', dbTypes);
+
+  // Запитуємо адреси для кожного типу DB
+  dbAdress = await askForDbAddress(dbTypes);
+
+  // Запитуємо prefix
+  prefix = await askForPrefix();
+  console.log('Введений prefix:', prefix);
+
+ // Після отримання адрес виконуємо основну логіку
+ ParseDBtoJson(dbfolder, jsonfolder, function (objects) {
+  // Викликаємо CreateFuxaTags і передаємо об'єкт
+  CreateFuxaTags(objects);
+
+  data[1].tags = tags; // Додаємо теги в загальний об'єкт data
+
+  fs.writeFileSync('./new.json', JSON.stringify(data, null, 2), 'utf8');
+  console.log("Оновлений data збережено у файл.");
+  
+});
+
+  rl.close(); // Закриваємо інтерфейс readline
+});
 
